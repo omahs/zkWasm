a22db5f5bac60f30fc07d2d9833c41bca1b2467e
Zhang Junyu <junyu92@gmail.com>
2023-06-06 08:57:54 +0800

    local_set

--- src/circuits/etable/op_configure/op_local_set.rs
+++ src/circuits/etable/op_configure/op_local_set.rs
@@ -1,5 +1,6 @@
 use crate::circuits::cell::*;
 use crate::circuits::etable::allocator::*;
+use crate::circuits::etable::stack_lookup_context::StackReadLookup;
 use crate::circuits::etable::ConstraintBuilder;
 use crate::circuits::etable::EventTableCommonConfig;
 use crate::circuits::etable::EventTableOpcodeConfig;
@@ -26,8 +27,7 @@ use specs::step::StepInfo;
 pub struct LocalSetConfig<F: FieldExt> {
     offset_cell: AllocatedCommonRangeCell<F>,
     is_i32_cell: AllocatedBitCell<F>,
-    value_cell: AllocatedU64Cell<F>,
-    memory_table_lookup_stack_read: AllocatedMemoryTableLookupReadCell<F>,
+    stack_reading_lookup: StackReadLookup<F>,
     memory_table_lookup_stack_write: AllocatedMemoryTableLookupWriteCell<F>,
 }
 
@@ -39,23 +39,15 @@ impl<F: FieldExt> EventTableOpcodeConfigBuilder<F> for LocalSetConfigBuilder {
         allocator: &mut EventTableCellAllocator<F>,
         constraint_builder: &mut ConstraintBuilder<F>,
     ) -> Box<dyn EventTableOpcodeConfig<F>> {
-        let is_i32_cell = allocator.alloc_bit_cell();
+        let mut stack_lookup_context = common_config.stack_lookup_context.clone();
+
+        let stack_reading_lookup = stack_lookup_context.pop(constraint_builder).unwrap();
+
+        let is_i32_cell = stack_reading_lookup.is_i32;
         let offset_cell = allocator.alloc_common_range_cell();
-        let value_cell = allocator.alloc_u64_cell();
 
         let sp_cell = common_config.sp_cell;
         let eid_cell = common_config.eid_cell;
-
-        let memory_table_lookup_stack_read = allocator.alloc_memory_table_lookup_read_cell(
-            "op_local_set stack read",
-            constraint_builder,
-            eid_cell,
-            move |____| constant_from!(LocationType::Stack as u64),
-            move |meta| sp_cell.expr(meta) + constant_from!(1),
-            move |meta| is_i32_cell.expr(meta),
-            move |meta| value_cell.u64_cell.expr(meta),
-            move |____| constant_from!(1),
-        );
 
         let memory_table_lookup_stack_write = allocator.alloc_memory_table_lookup_write_cell(
             "op_local_set stack write",
@@ -64,15 +56,14 @@ impl<F: FieldExt> EventTableOpcodeConfigBuilder<F> for LocalSetConfigBuilder {
             move |____| constant_from!(LocationType::Stack as u64),
             move |meta| sp_cell.expr(meta) + constant_from!(1) + offset_cell.expr(meta),
             move |meta| is_i32_cell.expr(meta),
-            move |meta| value_cell.u64_cell.expr(meta),
+            move |meta| stack_reading_lookup.value.u64_cell.expr(meta),
             move |____| constant_from!(1),
         );
 
         Box::new(LocalSetConfig {
             offset_cell,
             is_i32_cell,
-            value_cell,
-            memory_table_lookup_stack_read,
+            stack_reading_lookup,
             memory_table_lookup_stack_write,
         })
     }
@@ -99,17 +90,14 @@ impl<F: FieldExt> EventTableOpcodeConfig<F> for LocalSetConfig<F> {
                 depth,
                 value,
             } => {
-                self.is_i32_cell.assign(ctx, F::from(*vtype as u64))?;
-                self.value_cell.assign(ctx, *value)?;
                 self.offset_cell.assign(ctx, F::from(*depth as u64))?;
 
-                self.memory_table_lookup_stack_read.assign(
+                self.stack_reading_lookup.assign(
                     ctx,
                     entry.memory_rw_entires[0].start_eid,
                     step.current.eid,
                     entry.memory_rw_entires[0].end_eid,
                     step.current.sp + 1,
-                    LocationType::Stack,
                     *vtype == VarType::I32,
                     *value,
                 )?;
